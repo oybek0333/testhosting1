9e03e9ab44b6835e1a0ac4d169b58672b56902a5
oybek0333 <123316806+oybek0333@users.noreply.github.com>
2023-02-20 09:52:29 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,89 @@
+
+// Background  image change
+
+
+let btn1 = document.getElementById('btn-1');
+let btn2 = document.getElementById('btn-2');
+let btn3 = document.getElementById('btn-3');
+let btn4 = document.getElementById('btn-4');
+
+
+btn1.addEventListener('click', () => {
+    document.body.style.backgroundImage = "url('back1.jpg')";
+});
+
+btn2.addEventListener('click', () => {
+    document.body.style.backgroundImage = "url('bg1.jpg')";
+});
+
+btn3.addEventListener('click', () => {
+    document.body.style.backgroundImage = "url('bg2.jpg')";
+});
+
+btn4.addEventListener('click', () => {
+    document.body.style.backgroundImage = "url('field3.webp')";
+});
+
+// Background  image change
+
+
+
+
+
+
+// Background  image change time
+
+// var images = [
+//     'bg1.jpg',
+//     'bg2.jpg',
+//     'back1.jpg',
+//     'field1.jpg',
+//     'field2.webp',
+//     'field3.webp',
+//     'bg4.jpg',
+//     'bg3.jpg',
+// ]
+
+// var img = document.getElementById("img")
+
+// function imgDisp(num){
+    
+//     setInterval(function(){
+//         var num = Math.floor(Math.random()*8);
+//         img.style.backgroundImage = 'url("' + images[num] + '")';
+//         img.style.backgroundRepeat = "no-repeat";
+//     },900)
+   
+// }
+
+// imgDisp();
+
+// Background  image change time
+
+
+
+
+// Part image change
+
+// function changeImage(fileName){
+//  let img = document.querySelector("#bannerImage");
+//  img.setAttribute("src", fileName);
+
+// }
+// Part image change
+
+
+
+
+
+
+ 
+// Time change Images
+// let image = document.getElementById('image');
+// let images =['field1.jpg','field2.jfif', 'field3.jfif','back1.jpg'];
+// setInterval(function(){
+//     let random = Math.floor(Math.random() * 2);
+//     image.src = images[random];
+// },12200);
+// Time change Images
+
